2b5670ab1e9223bc278cd4893fd02aaa1943d2cc
Martin Macak <martin.macak@vertice.one>
2023-05-09 15:14:43 +0200

    wip

--- tests/test_yaml_extensions.py
+++ tests/test_yaml_extensions.py
@@ -4,36 +4,36 @@ import pytest
 
 
 @pytest.mark.parametrize('template_file_path, evaluate_macros, expected', [
-    # (
-    #         os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'simple_cf', 'template.yaml')),
-    #         True,
-    #         None,
-    # ),
-    # (
-    #         os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'complex_cf_01', 'template.yaml')),
-    #         True,
-    #         None,
-    # ),
-    # (
-    #         os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'simple_cf', 'template.yaml')),
-    #         False,
-    #         None,
-    # ),
-    # (
-    #         os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'complex_cf_01', 'template.yaml')),
-    #         False,
-    #         None,
-    # ),
+    (
+            os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'simple_cf', 'template.yaml')),
+            True,
+            None,
+    ),
+    (
+            os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'complex_cf_01', 'template.yaml')),
+            True,
+            None,
+    ),
+    (
+            os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'simple_cf', 'template.yaml')),
+            False,
+            None,
+    ),
+    (
+            os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'complex_cf_01', 'template.yaml')),
+            False,
+            None,
+    ),
     (
             os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'with_macros_01', 'template.yaml')),
             True,
             None,
     ),
-    # (
-    #         os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'with_macros_01', 'template.yaml')),
-    #         False,
-    #         None,
-    # ),
+    (
+            os.path.abspath(os.path.join(os.path.dirname(__file__), 'fixtures', 'with_macros_01', 'template.yaml')),
+            False,
+            None,
+    ),
 ])
 def test_load_cfn(template_file_path, evaluate_macros, expected):
     from cfn.yaml_extensions import load_cfn
